02feb358cbec185ea37b7a684ccbe8df7ccc1f0d
sajidimon <imonchowdhury6@gmail.com>
2024-06-08 08:54:59 +0600

    added delete user api

--- index.js
+++ index.js
@@ -65,6 +65,15 @@ async function run() {
             res.send(result)
         })
 
+        //delete user from db;
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
+
         //PRODUCT COLLECTION;
 
         //save products to db;
